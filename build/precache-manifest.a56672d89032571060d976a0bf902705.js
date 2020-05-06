10081242326f46e72a91952416a67ab2762865bf
Rodrigo Urban <rorikstark@gmail.com>
2020-05-06 01:12:32 -0300

    Added build

---
+++ build/precache-manifest.a56672d89032571060d976a0bf902705.js
@@ -0,0 +1,38 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ca378054a9393cdf8ed6072e436568ff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "312d7fdce9dc49bdbeba",
+    "url": "/static/css/2.d02fff44.chunk.css"
+  },
+  {
+    "revision": "cc7ba4adf8b377d62866",
+    "url": "/static/css/main.54d506dc.chunk.css"
+  },
+  {
+    "revision": "312d7fdce9dc49bdbeba",
+    "url": "/static/js/2.fdab3179.chunk.js"
+  },
+  {
+    "revision": "c3543651679f37448e670133f21082f5",
+    "url": "/static/js/2.fdab3179.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "cc7ba4adf8b377d62866",
+    "url": "/static/js/main.e19908ad.chunk.js"
+  },
+  {
+    "revision": "ccea2c94cb415da7fc18",
+    "url": "/static/js/runtime-main.b9a92d1d.js"
+  },
+  {
+    "revision": "2e7d700deb109da33cdbadd63c9ea950",
+    "url": "/static/media/llanero1.2e7d700d.jpg"
+  },
+  {
+    "revision": "e30c99a4816b37b6efed84e47bfe403d",
+    "url": "/static/media/llanerologo.e30c99a4.svg"
+  }
+]);
